4ea566a32a8ccc8a75b77bb446f42b876f4e6061
kchenfs <kendxd@gmail.com>
2023-09-24 11:51:51 -0400

    we dont need child modules

--- main.tf
+++ main.tf
@@ -405,22 +405,13 @@ resource "aws_lambda_function" "momotaro_function" {
 
   environment {
     variables = {
-      ITEM_PRICE_API_URL         = module.momotaro.get_item_info_url
-      MENU_ITEMS_API_URL         = module.momotaro.get_menu_items_url
-      SAVE_CUSTOMER_INFO_API_URL = module.momotaro.save_customer_info_url
+      ITEM_PRICE_API_URL         = ${aws_api_gateway_deployment.momotaro_deployment.invoke_url}/Momotaro/GetItemPrice/GET"
+      MENU_ITEMS_API_URL         = "${aws_api_gateway_deployment.momotaro_deployment.invoke_url}/Momotaro/GetMenuItems/GET"
+      SAVE_CUSTOMER_INFO_API_URL = "${aws_api_gateway_deployment.momotaro_deployment.invoke_url}/Momotaro/SaveCustomerInfo/POST"
       SNS_TOPIC_ID               = aws_sns_topic.my_topic.id # Add this line to reference the SNS topic ID
       # Add other environment variables as needed
     }
   }
-
-  # Other Lambda function configuration settings
-}
-
-
-module "momotaro" {
-  source = "git::https://github.com/kchenfs/Momotaro.git"
-
-  # Other module configuration settings go here
 }
 
 
