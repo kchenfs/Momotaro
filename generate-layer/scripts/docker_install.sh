04449b022a86bdb743c9cd66101bdab2023fb083
kchenfs <kendxd@gmail.com>
2023-09-29 09:42:06 -0400

    corrected the spacy version

--- generate-layer/scripts/docker_install.sh
+++ generate-layer/scripts/docker_install.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+# Exit on the first error
+set -e
+
+# Create a virtual environment
 virtualenv --python=/usr/bin/python3.11 python
 source python/bin/activate
-pip install -r requirements.txt -t python/lib/python3.11/site-packages
-python -m spacy download en_core_web_sm -t python/lib/python3.11/site-packages
-zip -r9 python.zip python
 
+# Install packages from requirements.txt
+pip install -r requirements.txt -t python/lib/python3.11/site-packages
+
+# Install spaCy
+python -m spacy download en_core_web_sm -t python/lib/python3.11/site-packages
+
+# Create a ZIP file
+zip -r9 python.zip python
